0951596f8d971d135808c20ca323db8947465424
nino <d20200720@gmail.com>
2022-03-09 13:32:42 +0900

    fix(markdown-editor): 動かないバグを修正

--- nextjs/demos/markdown-editor.tsx
+++ nextjs/demos/markdown-editor.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { storage } from '../firebase/client';
 
 const MarkdownEditor = () => {
-  const [value, setValue] = useState<string>();
+  const [_, setValue] = useState<string>();
 
   const uploadImage = async (file: File): Promise<string> => {
     const imageRef = ref(storage, `images/markdown-editor/${Date.now()}`);
@@ -13,32 +13,37 @@ const MarkdownEditor = () => {
     return getDownloadURL(imageRef);
   };
 
+  const onChange = useCallback((value: string) => {
+    setValue(value);
+  }, []);
+
+  const options = useMemo(() => {
+    return {
+      toolbar: [
+        'bold',
+        'italic',
+        'heading',
+        '|',
+        'quote',
+        'unordered-list',
+        'ordered-list',
+        '|',
+        'link',
+        'image',
+      ],
+      status: false,
+      spellChecker: false,
+      imageUploadFunction(file, onSuccess) {
+        if (file.type.match('image')) {
+          uploadImage(file).then((url) => onSuccess(url));
+        }
+      },
+    } as EasyMDE.Options;
+  }, []);
+
   return (
     <div className="relative">
-      <SimpleMDE
-        onChange={setValue}
-        options={{
-          toolbar: [
-            'bold',
-            'italic',
-            'heading',
-            '|',
-            'quote',
-            'unordered-list',
-            'ordered-list',
-            '|',
-            'link',
-            'image',
-          ],
-          status: false,
-          spellChecker: false,
-          imageUploadFunction(file, onSuccess) {
-            if (file.type.match('image')) {
-              uploadImage(file).then((url) => onSuccess(url));
-            }
-          },
-        }}
-      />
+      <SimpleMDE onChange={onChange} options={options} />
     </div>
   );
 };
