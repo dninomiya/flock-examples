02262888c39d82785c3f182db546926830643c71
nino <daichi.ninomiya@deer.co.jp>
2022-03-11 00:43:49 +0900

    fix(site): GitHub URL修正

--- nextjs/lib/site.ts
+++ nextjs/lib/site.ts
@@ -6,7 +6,7 @@ export const Site = {
       : 'https://examples.flock.codes',
   twitter: 'https://twitter.com/d151005',
   youtube: 'https://www.youtube.com/channel/UCUPq5dKFGnOziaqYI-ejYcg',
-  github: 'https://github.com/flock-team/demos',
+  github: 'https://github.com/dninomiya/flock-examples',
   flock: 'https://flock.codes/about',
   repository: 'https://github.com/dninomiya/flock-examples',
 };
