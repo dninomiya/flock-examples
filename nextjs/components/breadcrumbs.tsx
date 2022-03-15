33fbd067251bce5d02b4582f381054f70a692e22
nino <daichi.ninomiya@deer.co.jp>
2022-03-15 14:25:52 +0900

    fix(breadcumbs): パンくずのホームURL変更

--- nextjs/components/breadcrumbs.tsx
+++ nextjs/components/breadcrumbs.tsx
@@ -18,7 +18,7 @@ const Breadcrumbs = ({ pages }: Props) => {
     <nav className="flex" aria-label="Breadcrumb">
       <ol role="list" className="flex items-center space-x-4">
         <li>
-          <Link href="/">
+          <Link href="/guides">
             <a className="block text-gray-400 hover:text-gray-500">
               <HiHome className="flex-shrink-0 h-5 w-5" aria-hidden="true" />
               <span className="sr-only">ホーム</span>
